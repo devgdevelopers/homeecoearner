d83e62b6f9f2fef74ed784ced994de0c57c7a11f
neha-w3era <155545025+neha-w3era@users.noreply.github.com>
2024-08-13 11:03:56 +0530

    final changes for live

--- app/component/UserProfile.js
+++ app/component/UserProfile.js
@@ -52,8 +52,8 @@ const UserProfile = () => {
     <>
       <section className="bg-green">
         <div className="container mx-auto" key={product._id}>
-          <div className="flex flex-col md:flex-row py-10 gap-10">
-            <div className="flex justify-center items-center w-full md:w-[40%] ">
+          <div className="flex flex-col lg:flex-row py-10 gap-10">
+            <div className="flex justify-center items-center w-full lg:w-[40%] ">
               <div className="product-img-bg w-[360px] h-[330px] p-10 my-auto flex">
                 <Image
                   src={product.cardImg}
@@ -64,7 +64,7 @@ const UserProfile = () => {
                 />
               </div>
             </div>
-            <div className="w-[90%] md:w-[60%] flex justify-center gap-2 flex-col mx-auto text-center md:text-left ">
+            <div className="w-[90%] lg:w-[60%] flex justify-center gap-2 flex-col mx-auto text-center md:text-left ">
               <span
                 className="text-[36px] lg:text-[52px] font-extrabold text-white"
                 dangerouslySetInnerHTML={{ __html: product.cardHeading }}
@@ -86,15 +86,15 @@ const UserProfile = () => {
         <h1 className="text-[30px] md:text-[50px] text-green text-center font-bold pb-10">
           Key Features
         </h1>
-        <div className="flex flex-col md:flex-row  ">
-        <div className="w-[90%] md:w-[60%] mx-auto">
+        <div className="flex flex-col lg:flex-row  ">
+        <div className="w-[90%] lg:w-[60%] mx-auto">
   <span
     className="text-[18px] text-black "
     dangerouslySetInnerHTML={renderCustomizedContent(product.content)}
   />
 </div>
 
-          <div className="flex flex-col justify-between items-center p-10 w-full md:w-[40%]">
+          <div className="flex flex-col justify-between items-center p-10 w-full lg:w-[40%]">
   {product.images && product.images.length > 0 ? (
     product.images.map((image, index) => (
       image ? ( // Check if image is truthy (not null or undefined)
