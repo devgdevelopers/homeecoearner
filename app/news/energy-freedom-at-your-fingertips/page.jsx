806226a1c728e61d69bf01502ccce0e0b0dea658
neha-w3era <155545025+neha-w3era@users.noreply.github.com>
2024-08-16 13:36:00 +0530

    final changes for live

--- app/news/energy-freedom-at-your-fingertips/page.jsx
+++ app/news/energy-freedom-at-your-fingertips/page.jsx
@@ -7,10 +7,10 @@ const page = () => {
       <section className=" w-full  flex ">
         <div className="container mx-auto flex flex-col lg:flex-row py-10">
           <div className="w-full lg:w-1/2 gap-7 flex flex-col  justify-center p-3">
-            <h1 className="text-xl md:text-[50px] font-extrabold text-[#15A135] leading-tight ">
+            <h1 className="text-xl md:text-[42px] font-extrabold text-[#15A135] leading-tight ">
               Energy Freedom at Your Fingertips!
             </h1>
-            <p className="text-[19px] font-medium text-[#525C60]">
+            <p className="text-[18px] font-medium text-[#525C60]">
               Are you tired of high energy bills?
               <br />
               Looking for a solution that benefits both your wallet and the
@@ -33,31 +33,31 @@ const page = () => {
         </div>
       </section>
 
-      <div className="container mx-auto">
-        <div className="flex flex-col gap-3  mb-7">
-          <h2 className="text-xl p-4 md:text-[25px] font-bold text-[#15A135] ">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col gap-3  mb-5">
+          <h2 className="text-[20px] md:text-[25px] font-semibold md:font-bold text-[#15A135] ">
             Imagine having a Home EcoEarner Power System installed in your home.
             With no upfront or net cost to you, this revolutionary system offers
             a host of benefits you won’t want to miss.
           </h2>
         </div>
 
-        <div className="flex flex-col gap-3 px-5 ">
+        <div className="flex flex-col gap-3  ">
           <h2 className="text-xl font-bold text-[#15A135] ">
             Cost-Free Electricity
           </h2>
-          <p className=" font-medium text-[#525C60] ml-7 mb-7">
+          <p className=" font-medium text-[#525C60] mb-7">
             Say goodbye to soaring energy bills! Home EcoEarner allows you to
             generate cost-free electricity, up to the average annual UK
             household usage. Your home will be powered by clean, renewable
             energy saving you money while reducing your carbon footprint.
           </p>
         </div>
-        <div className="flex flex-col gap-3 px-5 ">
+        <div className="flex flex-col gap-3 ">
           <h2 className="text-xl  font-bold text-[#15A135] ">
             Extra Income
           </h2>
-          <p className=" font-medium text-[#525C60] ml-7 mb-7">
+          <p className=" font-medium text-[#525C60] mb-7">
             But that’s not all With Home EcoEarner, you can also earn an extra
             income. By harnessing the power of renewable energy, you can
             generate surplus electricity that can be sold back to the grid.
--- app/news/unleash-the-power-of-your-home/page.jsx
+++ app/news/unleash-the-power-of-your-home/page.jsx
@@ -6,13 +6,13 @@ const page = () => {
   return (
     <>
       <section className=" w-full">
-      <div className="container mx-auto flex flex-col xl:flex-row items-center pt-16 pb-16">
+      <div className="container mx-auto flex flex-col xl:flex-row items-center pt-16 pb-16 gap-10">
       <div className="w-[90%] xl:w-2/3 gap-7 flex flex-col mx-auto">
-          <h1 className="text-xl lg:text-[50px] font-extrabold text-[#15A135] leading-tight">
+          <h1 className="text-xl xl:text-[42px] font-extrabold text-[#15A135] leading-tight">
             Unleash the Power of Your Home: Turn Sunshine into Cash with Solar
             and Battery Systems!
           </h1>
-          <p className="text-[19px] font-medium text-[#525C60]">
+          <p className="text-[18px] font-medium text-[#525C60]">
             In a world where energy costs are soaring and environmental concerns
             are at the forefront, imagine a solution that not only benefits your
             wallet but also contributes to a greener planet. Welcome to the
@@ -37,10 +37,10 @@ const page = () => {
       <section className="md:h-full w-full md:px-[121px] flex flex-col pb-10">
       <div className="container mx-auto">
       <div className="flex flex-col gap-3 my-4">
-          <h1 className="text-xl md:text-[25px] pl-5 md:pl-0 font-extrabold text-[#15A135] ">
+          <h1 className="text-xl md:text-[22px] pl-5 md:pl-0 font-extrabold text-[#15A135] ">
             1. The Problem: Soaring Energy Bills and Environmental Worries
           </h1>
-          <article className="text-[19px] font-medium text-[#525C60] ml-7 mb-7">
+          <article className="text-[18px] font-medium text-[#525C60] ml-7 mb-7">
             We have all experienced the sting of rising energy bills, wondering
             how we can possibly keep up with the costs while still maintaining a
             comfortable lifestyle. Fossil fuels, the primary source of
@@ -52,10 +52,10 @@ const page = () => {
 
         
         <div className="flex flex-col gap-3 my-4">
-          <h1 className="text-xl md:text-[25px] font-extrabold pl-5 md:pl-0 text-[#15A135] ">
+          <h1 className="text-xl md:text-[22px] font-extrabold pl-5 md:pl-0 text-[#15A135] ">
             2. The Solution: Solar and Battery Systems – Your Ticket to Energy Independence
           </h1>
-          <article className="text-[19px] font-medium text-[#525C60] ml-7 mb-7">
+          <article className="text-[18px] font-medium text-[#525C60] ml-7 mb-7">
           Enter solar and battery systems – a revolutionary solution that not only slashes your electricity bills but also empowers you to be part of the clean energy revolution.
           By harnessing the power of the sun, you can generate your electricity, reducing reliance on the grid and minimising your carbon footprint.
           </article>
@@ -63,30 +63,30 @@ const page = () => {
 
         
         <div className="flex flex-col gap-3 my-4">
-          <h1 className="text-xl md:text-[25px] pl-5 md:pl-0 font-extrabold text-[#15A135] ">
+          <h1 className="text-xl md:text-[22px] pl-5 md:pl-0 font-extrabold text-[#15A135] ">
             3. Visualise the Benefits: See the Savings, Witness the Impact
           </h1>
           <div className="flex flex-col ml-7 ">          
             <h2 className="text-[20px]  font-semibold text-[#385723] ">• Monthly Savings:</h2>
-          <article className="text-[19px] font-medium text-[#525C60]  ">
+          <article className="text-[18px] font-medium text-[#525C60]  ">
     Picture a scenario - where your monthly electricity bill is significantly reduced, if not eliminated entirely.
 The money you save can be redirected towards covering other living costs or even that dream vacation you've been planning.</article>
 </div>
           <div className="flex flex-col ml-7 ">          
             <h2 className="text-[20px] font-semibold text-[#385723] ">• Monthly Income:</h2>
-          <article className="text-[19px] font-medium text-[#525C60]  ">
+          <article className="text-[18px] font-medium text-[#525C60]  ">
           Picture the next scenario - where you don’t have a monthly bill – you have a monthly income.
           The money you earn can be redirected towards topping up your pension savings or more meaningful investments.</article>
 </div>
           <div className="flex flex-col ml-7 ">          
             <h2 className="text-[20px] font-semibold text-[#385723] ">• Environmental Impact:</h2>
-          <article className="text-[19px] font-medium text-[#525C60]  ">
+          <article className="text-[18px] font-medium text-[#525C60]  ">
           Visualise your carbon footprint shrinking as you rely on clean, renewable energy.
           Each unit of electricity generated from your solar panels is a step towards a healthier planet.</article>
 </div>
           <div className="flex flex-col ml-7 ">          
             <h2 className="text-[20px] font-semibold text-[#385723] ">• Energy Independence:</h2>
-          <article className="text-[19px] font-medium text-[#525C60]  ">
+          <article className="text-[18px] font-medium text-[#525C60]  ">
           Imagine the peace of mind that comes with knowing you have a reliable source of energy, irrespective of external factors.
           Power outages become a thing of the past as your home becomes a fortress of energy independence.</article>
 </div>
@@ -95,37 +95,37 @@ The money you save can be redirected towards covering other living costs or even
 
         
         <div className="flex flex-col gap-3 my-4">
-          <h1 className="text-xl md:text-[25px] font-extrabold pl-5 md:pl-0 text-[#15A135] ">
+          <h1 className="text-xl md:text-[22px] font-extrabold pl-5 md:pl-0 text-[#15A135] ">
             4. Call to Action: Transform Your Home into a Powerhouse
           </h1>
           <div className="flex flex-col ml-7 ">          
             <h2 className="text-[20px] font-semibold text-[#385723] ">• Consultation:</h2>
-          <article className="text-[19px] font-medium text-[#525C60]  ">
+          <article className="text-[18px] font-medium text-[#525C60]  ">
           Reach out to Home EcoEarner by signing up on our Home page for a personalised consultation. Understand the potential savings and benefits tailored to your home's unique needs.</article>
 </div>
           <div className="flex flex-col ml-7 ">          
             <h2 className="text-[20px] font-semibold text-[#385723] ">• Financing Options:</h2>
-          <article className="text-[19px] font-medium text-[#525C60]  ">
+          <article className="text-[18px] font-medium text-[#525C60]  ">
           Explore various financing options, including government incentives and tax credits, to make the transition to solar and battery systems more affordable.</article>
 </div>
           <div className="flex flex-col ml-7 ">          
             <h2 className="text-[20px] font-semibold text-[#385723] ">• Installation:</h2>
-          <article className="text-[19px] font-medium text-[#525C60]  ">
+          <article className="text-[18px] font-medium text-[#525C60]  ">
           Take the plunge and get your solar and battery systems installed. Watch as your home transforms into a sustainable, money- saving powerhouse.</article>
 </div>
           <div className="flex flex-col ml-7 ">          
             <h2 className="text-[20px] font-semibold text-[#385723] ">• Spread the Word:</h2>
-          <article className="text-[19px] font-medium text-[#525C60]  ">
+          <article className="text-[18px] font-medium text-[#525C60]  ">
           Share your success story with friends and family, inspiring others to join the clean energy revolution.</article>
 </div>
         </div>
 
         
         <div className="flex flex-col gap-3 my-4">
-          <h1 className="text-xl md:text-[25px] font-extrabold text-[#15A135] pl-5 md:pl-0">
+          <h1 className="text-xl md:text-[22px] font-extrabold text-[#15A135] pl-5 md:pl-0">
             5. Conclusion: Embrace the Power, Reap the Rewards
           </h1>
-          <article className="text-[19px] font-medium text-[#525C60] ml-7 mb-7">
+          <article className="text-[18px] font-medium text-[#525C60] ml-7 mb-7">
           By turning your home into a micro power plant, you are not just saving money.
 You maybe even earning money and investing in a cleaner, more sustainable future.
 The benefits extend beyond your pocketbook to the global fight against climate change.
